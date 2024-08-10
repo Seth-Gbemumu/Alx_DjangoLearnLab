331deb2b9512e933fc47fe6482764f2822106301
seth-gbemumu <gracesefor@gmail.com>
2024-08-10 18:05:17 +0000

    Django Admin

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
-    list_filter = ("title", "author")
+    search_field = ("title", "author")
+    list_filter = ("publication_year")
 admin.site.register(Book, BookAdmin)
