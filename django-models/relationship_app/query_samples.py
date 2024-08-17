30ab68b7ffa6e72f0c87799e082cdfebafc03ec2
seth-gbemumu <gracesefor@gmail.com>
2024-08-17 07:50:39 +0000

    Django models

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -6,7 +6,7 @@ books_by_author = Book.objects.filter(author=author)
 print(f"Books by {author.name}: {list(books_by_author)}")
 
 #List books in a library
-library = Library.objects.get(name= "library_name")
+library = Library.objects.get(name="library_name")
 books_in_library = library.books.all()
 print(f"Books in {library.name}: {list(books_in_library)}")
 
