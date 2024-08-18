5ab1593d23d3ccfcce18fe9e56c4ecad4b68358f
seth-gbemumu <gracesefor@gmail.com>
2024-08-18 00:24:25 +0000

    views project

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .view import book_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    context = {"book_list": books}
+    return render (request, "relationship_app/book_list.html", context)
+
+
+class LibraryDetail(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_name = "library"
+
+
+
+
+    
+
+
+
+    
