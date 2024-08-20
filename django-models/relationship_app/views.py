2be20cb87111cf8726b77e0dbbf2c9a3f8fc5e1e
seth-gbemumu <gracesefor@gmail.com>
2024-08-20 22:27:33 +0000

    views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -37,29 +37,24 @@ class register(CreateView):
     template_name = "relationship_app/register.html"
     success_url = reverse_lazy('login')
 
-def Admin(user):
+def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-def Librarian(user):
+def is_librarian(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
-def Member(user):
+def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-# Views
-@user_passes_test(Admin)
+# Role-Based Views
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(Librarian)
+@user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(Member)
+@user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
-
-
-
-
-    
+    return render(request, 'relationship_app/member_view.html')
