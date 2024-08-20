d4dd8ba939f818acdab059b6fe9d3420b0ae9e25
seth-gbemumu <gracesefor@gmail.com>
2024-08-20 10:48:31 +0000

    fix

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', views.Register.as_view, name='register'),
+    path('register/', views.Register, name='register'),
 ]
 
 
