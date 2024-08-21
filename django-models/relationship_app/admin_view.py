85dc5c013d67e38e33c9c3169232f95e868cf404
seth-gbemumu <gracesefor@gmail.com>
2024-08-21 07:36:04 +0000

    role

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -6,6 +6,7 @@ def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 
 
+
 @user_passes_test(is_admin)
-def librarian_view(request):
+def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
