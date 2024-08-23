0a92664bd5994593f8aa4b75f3385fd66c032848
seth-gbemumu <gracesefor@gmail.com>
2024-08-23 04:47:15 +0000

    new fixes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth import decorators
+
 
 # Create your views here.
 def list_books(request):
@@ -38,29 +38,22 @@ class register(CreateView):
     success_url = reverse_lazy('login')
 
 def is_admin(user):
-   # return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    #return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
 def is_member(user):
-    #return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
-    return user.is_authenticated and user.userprofile.role == 'Member'
-# Views
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
-
-
-
-
-    
+    return render(request, 'relationship_app/member_view.html')
