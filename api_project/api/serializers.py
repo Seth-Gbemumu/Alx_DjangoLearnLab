ae59a0aed89a1847ec16a08253d6cbed388b19a6
seth-gbemumu <gracesefor@gmail.com>
2024-09-01 04:44:24 +0000

    fix

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Book
 
 
-class BookSerializer(serializers.Modelserializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['titles', 'author']
